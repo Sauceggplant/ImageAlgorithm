51920a9e1ce15b31ad8a5b71934f4a13be493fc8
jacob <zhaozx19881105@163.com>
2024-04-20 11:43:11 +0800

    1.0

--- src/main/java/org/sauceggplant/ia/algorithm/About.java
+++ src/main/java/org/sauceggplant/ia/algorithm/About.java
@@ -27,7 +27,7 @@ public class About implements Algorithm {
         logger.info("菜单：关于");
         JOptionPane.showMessageDialog(iaPanel,
                 "图像算法\n" +
-                        "版本:\t1.0-SNAPSHOT\n" +
+                        "版本:\t1.0\n" +
                         "版权:\tzhaozx19881105@163.com",
                 "关于",
                 JOptionPane.INFORMATION_MESSAGE,
